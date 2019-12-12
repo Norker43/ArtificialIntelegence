e63b655631e449db43ab1fbecd324886a4631d0e
rollandor <55525799+rollandor@users.noreply.github.com>
2019-12-12 12:38:36 +0500

    update 12.12.19

--- Lab_2/Lab_2/LogicalInferenceMachine.cs
+++ Lab_2/Lab_2/LogicalInferenceMachine.cs
@@ -96,8 +96,13 @@ namespace Lab_2
             Rule selectedRule = new Rule();
             string name = AskQuestion();
             #endregion
+            
+            // 1. Обход антецедента(-ов) гипотез(-ы) и сбор фактов
+            // 2. Сбор правил, консеквенты которых, удовлетворяют фактам
+            // 3. Пока результат null, возвращаться на (1)
+            // 4. Присвоить результату список фактов, которые указывают на правило с вопросом
 
-            foreach (Rule rule in results)
+            foreach (Rule rule in results) // находим и запоминаем правила-ответ, которое выбрал пользователь 
             {
                 if (rule.Name == name)
                 {
@@ -105,15 +110,34 @@ namespace Lab_2
                 }
             }
 
-            List<string> result = null;
-
-            while (result == null)
+            List<string> startQuestions = new List<string>();
+            for (int i = 0; i < knowledgeBase.Rules.Count; i++)
             {
-                knowledgeBase.Rules = knowledgeBase.Rules.Except(ruleWorkList.ItWorked).ToList();
+                if (knowledgeBase.Rules[i].Consequent[0].Contains("?"))
+                {
+                    startQuestions.Add(RuleManager.Sequentialization(knowledgeBase.Rules[i].Consequent)[0]);
+                }
+            }
+            List<string> mainFactsRule = FindFactsInRules(selectedRule);
 
+            List<string> finalResults = new List<string>();
+            for(int i = 0; i < mainFactsRule.Count; i++)
+            {
+                for (int j = 0; j < startQuestions.Count; j++)
+                {
+                    if (mainFactsRule[i].Contains(startQuestions[j]))
+                    {
+                        finalResults.Add(mainFactsRule[i]);
+                    }
+                }
             }
 
-            dialog.Invoke(new Action(() => dialog.AppendText("Конец")));
+            for (int i = 0; i < finalResults.Count; i++)
+            {
+                dialog.Invoke(new Action(() => dialog.AppendText(finalResults[i] + "\n")));
+            }
+
+            dialog.Invoke(new Action(() => dialog.AppendText("\nКонец")));
         }
 
         private void Explanation(Rule rule)
@@ -242,7 +266,69 @@ namespace Lab_2
         #endregion
 
         #region Для обратного вывода
+        private string[] ReadFactsRec(XmlNode xmlNode)
+        {
+            List<string> facts = new List<string>();
 
+            foreach (XmlNode node in xmlNode.ChildNodes)
+            {
+                if (node.Name == "not") continue;
+
+                if (node.Name == "or" || node.Name == "and")
+                {
+                    facts.AddRange(ReadFactsRec(node));
+                }
+                else if (node.Name == "fact")
+                {
+                    facts.Add(node.InnerText);
+                }
+            }
+
+            return facts.ToArray();
+        }
+
+        private void FindFactsInRules(Rule selectedRule, ref List<string> mainListFacts)
+        {
+            string[] tempFacts = ReadFactsRec(selectedRule.Antecedent);
+            for (int i = 0; i < tempFacts.Length; i++) // проходимся по списку правил Базы Знаний
+            {
+                for (int j = 0; j < knowledgeBase.Rules.Count; j++) // сравниваем консеквент правила БЗ со списком 
+                {
+                    if (knowledgeBase.Rules[j].Consequent.Contains(tempFacts[i]))
+                    {
+                        FindFactsInRules(knowledgeBase.Rules[j], ref mainListFacts);
+                    }
+                }
+                if (!mainListFacts.Contains(tempFacts[i]))
+                {
+                    mainListFacts.Add(tempFacts[i]);
+                }               
+
+            }
+        }
+
+        private List<string> FindFactsInRules(Rule selectedRule)
+        {
+            List<string> tempList = new List<string>();
+            string[] tempFacts = ReadFactsRec(selectedRule.Antecedent);
+            for (int i = 0; i < tempFacts.Length; i++) // проходимся по списку правил Базы Знаний
+            {
+                for (int j = 0; j < knowledgeBase.Rules.Count; j++) // сравниваем консеквент правила БЗ со списком 
+                {
+                    if (knowledgeBase.Rules[j].Consequent.Contains(tempFacts[i]))
+                    {
+                        tempList.AddRange(FindFactsInRules(knowledgeBase.Rules[j]));
+                    }
+                }
+
+                if (!tempList.Contains(tempFacts[i]))
+                {
+                    tempList.Add(tempFacts[i]);
+                }
+            }
+
+            return tempList;
+        }
         #endregion
 
         #endregion
